7d92dd4539b714d850d306a1be84ff0f1cedabe7
Lars Lehtonen <lars.lehtonen@gmail.com>
2016-08-25 00:12:37 -0700

    plausible/generator to create plausible MAC addresses

---
+++ generator/generator.go
@@ -0,0 +1,132 @@
+package generator
+
+import (
+	"bufio"
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"io"
+	"math/rand"
+	"sort"
+	"strings"
+	"time"
+	"unicode"
+)
+
+type prefix [3]uint8
+type vendor map[string][]prefix
+type vendorRecord struct {
+	company string
+	prefix  prefix
+}
+
+const manufPath = "/usr/share/wireshark/manuf"
+
+var UnparseableLineError = errors.New("Unparseable vendor line.")
+var NoSuchCompanyError = errors.New("No such company.")
+
+// addPrefix takes a single prefix record and appends the prefix value
+// to the company key in the vendor map.
+func (v vendor) addPrefix(vr vendorRecord) {
+	cleanCompanyName := strings.TrimSpace(strings.ToLower(vr.company))
+	v[cleanCompanyName] = append(v[cleanCompanyName], vr.prefix)
+}
+
+// CompanyList returns a list of every company key in the vendor map.
+func (v vendor) CompanyList() []string {
+	companies := []string{}
+	for k, _ := range v {
+		companies = append(companies, k)
+	}
+	sort.Strings(companies)
+	return companies
+}
+
+// loadRecords reads a flat file database of the Wireshark manuf
+// format and loads it into the vendor map.
+func (v vendor) loadRecords(r io.Reader) (err error) {
+	scanner := bufio.NewScanner(r)
+	for scanner.Scan() {
+		line := scanner.Text()
+		record, err := parseLine(line)
+		if err == nil {
+			switch err {
+			case UnparseableLineError:
+				return nil
+			default:
+				return err
+			}
+		}
+		v.addPrefix(record)
+	}
+	return nil
+}
+
+// RandomMAC provides a randomly-generated MAC address from
+// a randomly chosen portion of a company's assigned MAC
+// address space.
+func (v vendor) RandomMAC(company string) (string, error) {
+	companyRecord, ok := v[company]
+	if !ok {
+		return "", NoSuchCompanyError
+	}
+	rand.Seed(time.Now().UTC().UnixNano())
+	length := len(companyRecord)
+	p := companyRecord[rand.Intn(length)]
+	return fmt.Sprintf("%02x:%02x:%02x:%02x:%02x:%02x", p[0], p[1], p[2],
+		rand.Intn(255), rand.Intn(255), rand.Intn(255)), nil
+}
+
+// parseLine reads an individual line from a Wireshark "manuf" format
+// database and returns a structured vendorRecord if it is able to parse
+// the line.
+func parseLine(line string) (vr vendorRecord, err error) {
+	companyLeftColumn := 9
+	prefixLength := 8
+	octetPosition := []int{
+		0,
+		3,
+		6,
+	}
+
+	sepPosition := []int{
+		2,
+		5,
+	}
+
+	if len(line) < prefixLength {
+		return vr, UnparseableLineError
+	}
+
+	for _, pos := range sepPosition {
+		if line[pos] != ':' {
+			return vr, UnparseableLineError
+		}
+	}
+
+	wsPosition := 8
+	if !unicode.IsSpace(rune(line[wsPosition])) {
+		return vr, UnparseableLineError
+	}
+
+	var prefix prefix
+	var company string
+
+	for k, v := range octetPosition {
+		octet, err := hex.DecodeString(line[v : v+2])
+		prefix[k] = uint8(octet[0])
+		if err != nil {
+			return vr, UnparseableLineError
+		}
+	}
+
+	company = string(line[companyLeftColumn:len(line)])
+	company = strings.Split(company, "#")[0]
+	company = strings.TrimSpace(company)
+
+	vr = vendorRecord{
+		company,
+		prefix,
+	}
+	return vr, nil
+}
---
+++ generator/generator_test.go
@@ -0,0 +1,82 @@
+package generator
+
+import (
+	"os"
+	"testing"
+)
+
+func TestAddVendor(t *testing.T) {
+	p := prefix{
+		uint8(10),
+		uint8(20),
+		uint8(30),
+	}
+	v := make(vendor)
+	vr := vendorRecord{
+		"test",
+		p,
+	}
+	v.addPrefix(vr)
+	t.Log(v)
+}
+
+func TestRandomMAC(t *testing.T) {
+	p := prefix{
+		uint8(10),
+		uint8(20),
+		uint8(30),
+	}
+	v := make(vendor)
+	vr := vendorRecord{
+		"test",
+		p,
+	}
+	v.addPrefix(vr)
+	t.Log(v.RandomMAC("test"))
+}
+
+func TestParseLine(t *testing.T) {
+	validLine := "00:00:02        BbnWasIn        # BBN (was internal usage only, no longer used)"
+	invalidLine := "this should fail"
+	t.Log("Parsing valid line.")
+	parsed, err := parseLine(validLine)
+	t.Log(parsed)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log("Parsing invalid line.")
+	_, err = parseLine(invalidLine)
+	if err == nil {
+		t.Fatalf("Invalid line \"%s\" should have failed to parse.", invalidLine)
+	}
+}
+
+func TestLoadVendor(t *testing.T) {
+	f, err := os.Open(manufPath)
+	if err != nil {
+		t.Fatal(err)
+	}
+	v := make(vendor)
+	v.loadRecords(f)
+	t.Log(v["google"])
+}
+
+func TestCompanyList(t *testing.T) {
+	vendor := make(vendor)
+	vendorLines := []string{
+		"00:00:08        FuturePn        # Officially Xerox, but 0:0:0:0:0:0 is more common",
+		"00:00:01        Superlan        # SuperLAN-2U",
+	}
+	for _, v := range vendorLines {
+		vr, err := parseLine(v)
+		if err != nil {
+			t.Fatal(err)
+		}
+		vendor.addPrefix(vr)
+	}
+	if len(vendor.CompanyList()) != len(vendorLines) {
+		t.Fatalf("Company list is %d, expected %d", len(vendor.CompanyList()), len(vendorLines))
+	} else {
+		t.Logf("Vendor map populated with %d entries, CompanyList() returned same.", len(vendor.CompanyList()))
+	}
+}
