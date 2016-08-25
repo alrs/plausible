f3109d81b387f21a96690ef38a128867862b2f41
Lars Lehtonen <lars.lehtonen@gmail.com>
2016-08-25 12:52:01 -0700

    main.go
    hide plausible binary from git

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"plausible/generator"
+)
+
+func printManufacturerList(m *generator.Manuf) {
+	companies := m.CompanyList()
+	for _, c := range companies {
+		fmt.Println(c)
+	}
+	os.Exit(0)
+}
+
+func main() {
+
+	listArg := flag.Bool("l", false, "List manufacturers.")
+	manuArg := flag.String("m", "google", "Select manufacturer.")
+	dbArg := flag.String("d", generator.ManufPath, "Path of database file.")
+
+	flag.Parse()
+
+	manuf, err := generator.NewManuf(*dbArg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if *listArg {
+		printManufacturerList(&manuf)
+	}
+
+	mac, err := manuf.RandomMAC(*manuArg)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(mac)
+}
